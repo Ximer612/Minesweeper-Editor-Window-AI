7e4e2c0552bdc1e23f23beacc3bb3c2236995c9a
Ximer - Marco <20438890+Ximer612@users.noreply.github.com>
2025-02-01 11:09:18 +0100

    Initial project

---
+++ Source/MinesweeperAI/MinesweeperAI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MinesweeperAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MinesweeperAI, "MinesweeperAI" );
